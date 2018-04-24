194ae99a33407583aaf061e7c53c2a5c653b4401
IEUser <IEUser@192.168.190.139>
2018-04-24 01:38:01 -0700

    version del wachin(biba wukong)

--- src/HelloGi.java
+++ src/HelloGi.java
@@ -4,6 +4,7 @@ public class HelloGi {
 	public static void main(String[] args) {
 		System.out.println("Hello Git Word");
 		System.out.println("Hello Git version2222");
-	}
-
+		System.out.println("Hello Git version3333");
+		
+}
 }
